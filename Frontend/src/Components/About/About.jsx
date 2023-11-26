93a8f64314eebe9192e1ad84ad6251c60e2fc113
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2023-11-26 15:32:37 +0530

    About Page Auth Removed

--- Frontend/src/Components/About/About.jsx
+++ Frontend/src/Components/About/About.jsx
@@ -1,20 +1,4 @@
-import React from 'react'
-import { tokenCheck } from '../../helperToken';
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-
 function About() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    let response = tokenCheck();
-    if (!response) {
-      navigate('/Login')
-    }
-    else {
-      navigate('/about');
-    }
-  }, [])
 
   return (
     <>
